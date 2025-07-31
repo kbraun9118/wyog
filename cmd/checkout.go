647b3b9ac1567b50b8c474f8029f660cc1f09220
Kurt Braun <kbraun@kbraun.dev>
2025-07-31 08:48:26 -0500

    checkout working

--- cmd/checkout.go
+++ cmd/checkout.go
@@ -1,7 +1,100 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/kbraun9118/wyog/gitobject"
+	"github.com/kbraun9118/wyog/repository"
+	"github.com/spf13/cobra"
+)
 
 var checkoutCmd = &cobra.Command{
-	Use: "checkout",
+	Use:   "checkout commit path",
+	Short: "Checkout a commit inside of a directory.",
+	Args:  cobra.ExactArgs(2),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		path, err := repository.FindRequire(".")
+		if err != nil {
+			return err
+		}
+
+		repo, err := repository.New(path)
+		if err != nil {
+			return err
+		}
+
+		commit, path := args[0], args[1]
+
+		obj, err := gitobject.Read(&repo, gitobject.Find(&repo, commit, "commit"))
+		if err != nil {
+			return err
+		}
+
+		if commitObj, ok := obj.(*gitobject.Commit); ok {
+			tree, ok := commitObj.Kvlm.Headers.Get("tree")
+			if !ok {
+				return fmt.Errorf("cannot find tree object")
+			}
+			obj, err = gitobject.Read(&repo, tree[0])
+		}
+
+		treeObj, ok := obj.(*gitobject.Tree)
+		if !ok {
+			return fmt.Errorf("")
+		}
+
+		if pathStat, err := os.Stat(path); err == nil {
+			if !pathStat.IsDir() {
+				return fmt.Errorf("Not a directory %s", path)
+			}
+			if pathDir, err := os.ReadDir(path); err == nil || len(pathDir) != 0 {
+				return fmt.Errorf("Not empty %s", path)
+			}
+		} else {
+			if err := os.MkdirAll(path, 0755); err != nil {
+				return fmt.Errorf("error creating directories")
+			}
+		}
+
+		absPath, err := filepath.Abs(path)
+		if err != nil {
+			return fmt.Errorf("cannot create absolute path")
+		}
+
+		if err := treeCheckout(&repo, treeObj, absPath); err != nil {
+			return err
+		}
+
+		return nil
+	},
+}
+
+func treeCheckout(repo *repository.Repository, tree *gitobject.Tree, path string) error {
+	for _, item := range tree.Items {
+		obj, err := gitobject.Read(repo, item.Sha)
+		if err != nil {
+			return err
+		}
+		dest := filepath.Join(path, item.Path)
+
+		switch objType := obj.(type) {
+		case *gitobject.Tree:
+			os.Mkdir(dest, 0755)
+			err := treeCheckout(repo, objType, dest)
+			if err != nil {
+				return err
+			}
+		case *gitobject.Blob:
+			err := os.WriteFile(dest, objType.Serialize(), 0644)
+			if err != nil {
+				return fmt.Errorf("cannot write file %s", dest)
+			}
+		default:
+			return fmt.Errorf("incorrect gitobject type")
+		}
+	}
+
+	return nil
 }
